63d9075d3bd4e91b2f213bdd91c57c8af3ee6263
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2023-12-05 17:46:24 +0100

    Day 2

--- src/day2.ts
+++ src/day2.ts
@@ -1,9 +1,7 @@
 import { getInputLines, printPart } from "./util";
 
 const day = 2;
-const lines = await getInputLines(day);
-
-const games = lines.map((line, i) => {
+const games = (await getInputLines(day)).map((line, i) => {
   const [game, sets] = line.split(":");
   return {
     sets: sets.split(";").map((set) =>
@@ -16,18 +14,38 @@ const games = lines.map((line, i) => {
   };
 });
 
-const cubeCounts = {
+const maxCubeCounts = {
   red: 12,
   green: 13,
   blue: 14,
 };
 
-const part1 = games
-  .filter(({ sets }) =>
-    sets.every((set) =>
-      set.every(({ count, color }) => count <= cubeCounts[color])
+printPart(
+  1,
+  games
+    .filter(({ sets }) =>
+      sets.every((set) =>
+        set.every(({ count, color }) => count <= maxCubeCounts[color])
+      )
     )
-  )
-  .reduce((acc, { index }) => acc + index, 0);
+    .reduce((acc, { index }) => acc + index, 0)
+);
 
-printPart(1, part1);
+printPart(
+  2,
+  games
+    .map(({ sets }) => {
+      const { red, green, blue } = sets.reduce(
+        (minCounts, set) => {
+          set.forEach(({ count, color }) => {
+            minCounts[color] = Math.max(minCounts[color] || 0, count);
+          });
+          return minCounts;
+        },
+        { red: 0, green: 0, blue: 0 }
+      );
+
+      return red * green * blue;
+    })
+    .reduce((acc, val) => acc + val, 0)
+);
